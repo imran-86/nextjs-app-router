c9843d430ec9a56c59f7cfd6cf7f8c25cb6d9872
student-Imran <ahmedimranmd55@gmail.com>
2025-11-28 12:47:39 +0600

    Final COmmit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true, // Required for static export
+  },
+}
+
+module.exports = nextConfig
